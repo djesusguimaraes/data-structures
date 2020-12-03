174a440f33f5b9436c70a43347afaf29eacf2789
djesusguimaraes <jesus.daniel@uft.edu.br>
2020-12-03 15:23:54 -0300

    3 de dezembro

--- array.py
+++ array.py
@@ -1,11 +1,12 @@
+from abc import abstractproperty
 import sys
 import random
 
 array = list(range(10000))
 
-
 for i in array:
     array[i] = i + 1
+random.shuffle(array)
 
 flag = 0
 arrayReversed = array.copy()
@@ -18,28 +19,30 @@ while flag < 25:
     print(arrayReversed[flag])
     flag += 1
 sys.stdout.close()
-
-
-cont = 1
-sys.stdout = open('arrayConjMenor.txt', 'w')
-while cont <= 50:
-    print(array[arrayMed - cont])
-    cont += 1
-sys.stdout.close()
    
+count = 0
+flag = 0
+sys.stdout = open('arrayConjMenor.txt', 'w')
+while flag < 50:
+    if arrayReversed[count] < arrayMed:
+        print(arrayReversed[count])
+        flag += 1
+    count += 1        
+sys.stdout.close()   
 
-cont = 0
+count = 0
+flag = 0
 sys.stdout = open('arrayConjMaior.txt', 'w')
-while cont < 50:
-    print(array[arrayMed + cont])
-    cont += 1
+while flag < 50:
+    if arrayReversed[count] > arrayMed:
+        print(arrayReversed[count])
+        flag += 1
+    count += 1        
 sys.stdout.close()
-
-random.shuffle(array)
 
 sys.stdout = open('arrayFull.txt', 'w')
-i = 0
-while i < int(len(array)):
-    print(array[i])
-    i += 1
+count = 0
+while count < int(len(array)):
+    print(array[count])
+    count += 1
 sys.stdout.close()
