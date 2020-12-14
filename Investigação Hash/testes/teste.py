e7326017eed7c801115a0c306a2f24f00077cb67
djesusguimaraes <jesus.daniel@uft.edu.br>
2020-12-14 20:47:39 -0300

    digitos deu bom

--- Investigação Hash/testes/teste.py
+++ Investigação Hash/testes/teste.py
@@ -2,8 +2,8 @@ from operator import itemgetter
 import operator
 import sys
 decimal = 10
-array = list(range(10))
-arquivo = open('input2.txt', 'r')
+array = list(range(500))
+arquivo = open('input.txt', 'r')
 i = 0 
 for linha in arquivo:
     array[i] = int(linha)
@@ -23,16 +23,11 @@ for i in range(decimal):
 i = 0
 while i < len(array):
     flag1[int(array[i] / pow(decimal, 3))] += 1
-
     flag2[int((array[i] / pow(decimal, 2)) % decimal)] += 1
-
     flag3[int((array[i] / decimal) % decimal)] += 1
-    
     flag4[int(array[i] % decimal)] += 1
-
     i += 1
 
-# fazer dict organizando os digitos d1, d2, d3, d4
 i = 0
 d1 = 0
 d2 = 0
@@ -45,3 +40,13 @@ while i < decimal:
     d4 += abs(flag4[i]-len(array)/decimal)  
     i += 1
 
+i = 0
+j = 0
+desvio = [d1, d2, d3, d4]
+digitos = list(range(3))
+aux = max(desvio)
+while i < len(desvio):
+    if desvio[i] < aux:
+        digitos[j] = i
+        j += 1
+    i += 1
